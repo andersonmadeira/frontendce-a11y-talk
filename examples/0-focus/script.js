e9d67ba695f4fbed65790bbd62e47f8811899678
Anderson Madeira <andersonmadeiracs@gmail.com>
2021-01-28 21:08:34 -0300

    feat: melhorias nos exemplos

--- examples/0-focus/script.js
+++ examples/0-focus/script.js
@@ -6,3 +6,22 @@ function closeSideMenu() {
   document.getElementById('side-menu').style.width = '0'
   document.query
 }
+
+const toggleMenuButton = document.getElementById('menu-toggle-button')
+const closeMenuButton = document.querySelector('.close-button')
+
+closeMenuButton.addEventListener('click', function () {
+  const menuItems = document.querySelectorAll('#side-menu a')
+
+  menuItems.forEach(item => {
+    item.setAttribute('tabindex', '-1')
+  })
+})
+
+toggleMenuButton.addEventListener('click', function () {
+  const menuItems = document.querySelectorAll('#side-menu a')
+
+  menuItems.forEach(item => {
+    item.setAttribute('tabindex', '0')
+  })
+})
